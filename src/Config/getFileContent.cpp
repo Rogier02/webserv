5157a7d6d499ebe9a753ffff4bd2466ac8f5f5cc
Laura de Koning <lade-kon@student.codam.nl>
2025-11-12 13:11:41 +0100

    Added a.out to .gitignore. Push before working on my own branch again. Oops.

--- src/Config/getFileContent.cpp
+++ src/Config/getFileContent.cpp
@@ -1,19 +1,25 @@
-#include "Config.hpp"
+#include "../../incl/Config.hpp"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
 
 void	printTokens(std::vector<Token> tokens){
-	for (int i = 0; i < tokens.size(); i++){
-		std::cout << i << " - Text: " << tokens[i].text << " - line: " << tokens[i].lineNbr << "\n";
+	for (size_t i = 0; i < tokens.size(); i++){
+		std::cout << i << "\t- line: " << tokens[i].lineNbr << "\t- Text: " << tokens[i].text << "\n";
 	}
 }
 
-
 void	getFileContent(std::string fileName){
-	if (fileName.size() >= 5 && fileName.rfind(".conf") != fileName.size() - 5;)
+	if (fileName.size() >= 5 && fileName.rfind(".conf") != fileName.size() - 5)
 		std::cout << "Error: incorrect file extension\n"; //return, error, exit?
 	std::ifstream file(fileName);
-	if (!file.isopen)
-		return -1; //give error message
-	std::string line = nullptr;
+	if (!file.is_open()){
+		std::cout << "Error: couldn't open file\n"; //return, error, exit?
+		return ; //give error message
+	}
+	std::string line = "";
 	std::vector<Token> tokens;
 	int lineNbr = 0;
 	while (std::getline(file, line)){
@@ -24,15 +30,41 @@ void	getFileContent(std::string fileName){
 
 		while (ss >> word){
 			if (line.empty() || word[0] == '#')
-				continue;
+				break;
 			tokens.push_back({word, lineNbr});
 		}
 	}
+	printTokens(tokens);
 	// now i have a lot of tokens.
 	// Next: do something with them/
-
+	Config config();
+	config.loadFromFile(tokens);
 }
 
-int	main(){
-	
+void	loadFromFile(std::vector<Token> tokens){
+	for (size_t i = 0; i < tokens.size(); i++){
+		if (tokens[i].text == "Server")
+			serverConfig(tokens, i);
+		if (tokens[i].text == "}")
+			return ;
+	}
+}
+
+void	serverConfig(std::vector<Token> tokens, size_t start){
+	for (size_t i = start; i < tokens.size(); i++){
+		if (tokens[i].text == "error_page")
+			errorPagesConfig(tokens, i);
+		if (tokens[i].text == "}")
+			return ;
+		else {
+			std::cout << 
+		}
+	}
+}
+
+int	main()
+{
+	getFileContent("default.conf");
+
+	return (0);
 }
